ac7e6ab94251eebd07a59be2e1e919e029b2bbb5
Yana Turiak <turyak1998@gmail.com>
2024-08-17 16:23:56 +0100

    fix shutdown hooks

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 const config = require('./config')
 const { connectDB, disconnectDB } = require('./db')
+const app = require('./app')
 
 connectDB(config.mongo.uri)
 
+const server = app.listen(config.server.port, () => {
+    console.log(`Server is running on port ${config.server.port}`)
+})
+
 // gracefull shutdown hook
-// when app receives termination signal, we close mongodb connection
-process.on('SIGINT', disconnectDB) 
+// when app receives termination signal, we close mongodb and server connections
+const gracefulShutdown = () => {
+    console.log('Received termination signal, shutting down...')
+    server.close(() => {
+        console.log('Server is now closed')
+        disconnectDB()
+    })
+}
+process.on('SIGINT', gracefulShutdown) 
+process.on('SIGTERM', gracefulShutdown) 
+
