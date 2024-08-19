f920b0df273072b6d457eb2cdb2d26bf6c786e14
Yana Turiak <turyak1998@gmail.com>
2024-08-19 17:34:23 +0100

    setup error-handlers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { getAllBooks } = require('./controllers/books')
 const { getEndpoints } = require('./controllers')
+const { serverErrorHandler } = require('./error-handlers')
 
 const app = express()
 app.use(express.json())
@@ -9,8 +10,12 @@ app.get('/api', getEndpoints)
 
 app.get('/api/books', getAllBooks)
 
+
+
 app.all('*', (req, res, next) => {
     res.status(404).send({message: 'path not found'})
 })
+
+app.use(serverErrorHandler)
 
 module.exports = app
--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,13 +1,13 @@
 const { findAllBooks } = require('../models/books')
 
-exports.getAllBooks = (req, res) => {
+exports.getAllBooks = (req, res, next) => {
     findAllBooks()  // Fetch all books from the database
     .then(books => {
         res.status(200).send({ books });
     })
     .catch(error => {
         console.error('Error fetching books:', error);
-        res.status(500).send({ msg: 'Internal Server Error' });
+        next(error)
     });
 };
 
