f91a13761b4419224eadf900447cfbaba33678ee
MoBel92 <belhadj.mohamad@gmail.com>
2024-08-19 14:25:27 +0100

    created Data for users

---
+++ backend/db/data/test/users.js
@@ -0,0 +1,34 @@
+[
+  {
+    username: "danleonard23",
+    name: "Daniel Leonard",
+    avatar: "avatar1",
+    email: "alice@example.com", // to be updated
+    password: "hashedpassword123",
+    location: "New York, USA",
+  },
+  {
+    username: "BelMo92",
+    name: "Belhaj Mohamed",
+    avatar: "avatar2",
+    email: "belhadj.mohamad@gmail.com",
+    password: "hashedpassword456",
+    location: "London, UK",
+  },
+  {
+    username: "anitacampbell17",
+    name: "Anita Campbell",
+    avatar: "avatar3",
+    email: "charlie@example.com", // to be updated
+    password: "hashedpassword789",
+    location: "Sydney, Australia",
+  },
+  {
+    username: "yana53674808",
+    name: "Yana",
+    avatar: "avatar4",
+    email: "diana@example.com", // to be updated
+    password: "hashedpassword101",
+    location: "Toronto, Canada",
+  },
+];
