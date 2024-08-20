696f2bf315985e63c687c4d868fc1ff4c0fed654
MoBel92 <belhadj.mohamad@gmail.com>
2024-08-20 15:03:33 +0100

    created swapCollections table and seed it

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const { getAllBooks } = require('./controllers/books')
-const { getEndpoints } = require('./controllers')
-const { serverErrorHandler } = require('./error-handlers')
+const express = require("express");
+const { getAllBooks } = require("./controllers/books");
+const { getEndpoints } = require("./controllers");
+const { serverErrorHandler } = require("./error-handlers");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.get('/api', getEndpoints)
+app.get("/api", getEndpoints);
 
-app.get('/api/books', getAllBooks)
- 
-app.all('*', (req, res, next) => {
-    res.status(404).send({message: 'path not found'})
-})
+app.get("/api/books", getAllBooks);
 
-app.use(serverErrorHandler)
+app.all("*", (req, res, next) => {
+  res.status(404).send({ message: "path not found" });
+});
 
-module.exports = app
+app.use(serverErrorHandler);
+
+module.exports = app;
