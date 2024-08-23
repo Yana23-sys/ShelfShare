9e43146c097499b57853b440388c2cabe25d6c43
Yana Turiak <turyak1998@gmail.com>
2024-08-23 18:11:42 +0100

    Enable cors

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const { getAllBooks, createBook, getBookById } = require('./controllers/books')
 const { getEndpoints } = require('./controllers')
 const { serverErrorHandler } = require('./error-handlers')
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get("/api", getEndpoints);
 
