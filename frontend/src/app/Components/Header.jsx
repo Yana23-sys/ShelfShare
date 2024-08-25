e43710664c3eefdfbd464cf44e040fac1fb1ad8b
Yana Turiak <turyak1998@gmail.com>
2024-08-25 13:12:44 +0100

    refactor navbar

--- frontend/src/app/Components/Header.jsx
+++ frontend/src/app/Components/Header.jsx
@@ -1,16 +1,36 @@
 "use client";
-import Navbar from "./Navbar";
+import Link from "next/link";
 import styles from "../Styles/Header.module.css";
 import { useContext } from "react";
 import { UserContext } from "../Contexts/UserContext";
 
 const Header = () => {
    const { user, setUser } = useContext(UserContext);
+   const handleLogout = () => {
+      setUser({});
+   };
 
    return (
       <header className={styles.header}>
-         <h1>ShelfShare</h1>
-         <Navbar />
+         <Link href="/" prefetch={false}>
+            <h1>ShelfShare</h1>
+            </Link>
+
+         <nav className={styles.navbar}>
+            <Link href="/books" prefetch={false}>
+               Browse Books
+            </Link>
+            <Link href="/post-book" prefetch={false}>
+               Add book
+            </Link>
+            {!user.username && <Link href="/login" prefetch={false}>
+               Login
+            </Link>}
+            {user.username && <Link onClick={handleLogout} href="/" prefetch={false}>
+               Logout
+            </Link>}
+         </nav>
+
          {user.username && (
             <img
                className={styles.avatarImg}
