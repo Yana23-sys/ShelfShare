f0efc1a03ebfa9c86eedeaa7740971a72f1ab74f
Yana Turiak <turyak1998@gmail.com>
2024-08-25 11:44:20 +0100

    Adjust booklist styling

--- frontend/src/app/Components/Booklist.jsx
+++ frontend/src/app/Components/Booklist.jsx
@@ -3,7 +3,7 @@ import { Grid } from '@mui/material';
   
 const Booklist = ({ books }) => {  
   return (  
-    <Grid container spacing={4}>  
+    <Grid container spacing={4} style={{ padding: "20px" }}>  
       {books.map((book, index) => (  
         <Grid item key={index} xs={12} sm={6} md={4} lg={3}>  
           <BookCard book={book} />  
