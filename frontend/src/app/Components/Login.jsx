6cb9f3d974c65d6e63be1c62e0efb7992f3fa4ff
Yana Turiak <turyak1998@gmail.com>
2024-08-26 12:21:04 +0100

    fetch users books from BE

--- frontend/src/app/Components/Login.jsx
+++ frontend/src/app/Components/Login.jsx
@@ -1,50 +1,95 @@
 "use client";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../Contexts/UserContext";
 import styles from "../Styles/Login.module.css";
 import Link from "next/link";
-
+import { TextField, Button, Container, Box, Typography } from "@mui/material";
 
 function Login({ users }) {
-   const { user, setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const handleLogin = (user) => {
-      setUser(user);
-      console.log(user.username);
-   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your login logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
 
-   return (
-      <div>
-         <h2>Enter your crendentials to login</h2>
-         <form className={styles.loginFormContainer}>
-            <label className={styles.loginEmailLabel} htmlFor="login">
-               Email
-               <input placeholder="Enter your email address"></input>
-            </label>
-            <label className={styles.loginPasswordLabel}>
-               Password
-               <input placeholder="Enter your password"></input>
-            </label>
-            <button className={styles.loginButton}>Login</button>
-         </form>
-         <h2>
-            Or click on an existing user profile to test some functionalities
-         </h2>
-         <section className={styles.userButtonsContainer}>
-            {users.map((user) => (
-               <button
-                  onClick={ () => handleLogin(user) }
-                  className={styles.userLoginButton}
-               >
-                  <Link href="/" prefetch={false}>
-                     {user.username}
-                     <img src={user.avatar} />
-                  </Link>
-               </button>
-            ))}
-         </section>
-      </div>
-   );
+  const handleLogin = (user) => {
+    setUser(user);
+    console.log(user.username);
+  };
+
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: 8,
+        }}
+      >
+        <Typography component="h2" variant="h5">
+          Enter your crendentials to login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+
+      <Typography component="h2" variant="h5" style={{ margin: "2rem" }}>
+        Or click on an existing user profile to test some functionalities
+      </Typography>
+      <section className={styles.userButtonsContainer}>
+        {users.map((user) => (
+          <button
+            key={user._id}
+            onClick={() => handleLogin(user)}
+            className={styles.userLoginButton}
+          >
+            <Link href="/" prefetch={false}>
+              {user.username}
+              <img src={user.avatar} />
+            </Link>
+          </button>
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/app/Contexts/UserContext.jsx
+++ frontend/src/app/Contexts/UserContext.jsx
@@ -1,9 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { getAllBooksByUserId } from "../api/books";
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
    const [user, setUser] = useState({});
+
+   useEffect(() => {
+      if (user._id) {
+         getAllBooksByUserId(user._id)
+         .then((books) => setUser({...user, books}))
+      }
+   }, [user._id]);
+   // TODO: fetch users swap requests
 
    return (
       <UserContext.Provider value={{ user, setUser }}>
