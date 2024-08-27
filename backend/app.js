1b6b61e7b2770fd9d3faf0c01177fcbdb2053a9a
NeetzBeatz <anitaleahcampbell@hotmail.co.uk>
2024-08-27 11:29:03 +0100

    ticket-13 completed - backend creatinf swap requests

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { getAllBooks, createBook, getBookById } = require('./controllers/books')
+const { getAllBooks, createBook, getBookById} = require('./controllers/books')
 const { getAllUsers } = require('./controllers/users')
 const { getEndpoints } = require('./controllers')
 const { serverErrorHandler } = require('./error-handlers')
 const cors = require('cors')
+const { createBookSwap } = require('./controllers/swaps')
 
 const app = express();
 app.use(express.json());
@@ -16,7 +17,8 @@ app.get('/api/users', getAllUsers)
 app.get('/api/books', getAllBooks)
 app.post('/api/books', createBook)
 app.get("/api/books/:bookId", getBookById)
- 
+app.post("/api/swaps", createBookSwap)
+
 app.all('*', (req, res, next) => {
     res.status(404).send({message: 'path not found'})
 })
