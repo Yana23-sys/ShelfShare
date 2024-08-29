a0e770c4489d9b3cee270179699d64d4b9df4c1d
NeetzBeatz <anitaleahcampbell@hotmail.co.uk>
2024-08-29 11:35:22 +0100

    minor changes made to css and layout - avatar icon updated in navbar and map resized

--- frontend/src/app/Components/Map.jsx
+++ frontend/src/app/Components/Map.jsx
@@ -4,8 +4,8 @@ import styles from "../Styles/Map.module.css";
 const Map = () => {
   return (
     <Link href="/books/66ced211cd7a0381694b545a" prefetch={false}>
-    <div className={styles.mapContainer}>
-    <iframe className={styles.map} width="900" height="580" src="https://api.maptiler.com/maps/5c2aff68-c608-459b-9214-fd31c158628c/?key=thOdTAyGPF7S1OunajFd#5.0/52.50740/0.02780"></iframe>
+    <div>
+    <iframe className={styles.map} width="450" height="280" src="https://api.maptiler.com/maps/5c2aff68-c608-459b-9214-fd31c158628c/?key=thOdTAyGPF7S1OunajFd#5.3/52.50740/0.02780"></iframe>
     </div>
     </Link>
   );
